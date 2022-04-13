54cb25e147b474546c7819bc5e452545d1df63d1
Milena <elenprojects7@gmail.com>
2022-04-13 15:06:42 -0300

    update url

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link",
+    url="https://github.com/Konstructa/my-package-python",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8'
